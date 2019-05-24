44a6707438aa05fc403dedd78756d963a0b3fcb9
brianmanderson <bmanderson@mdanderson.org>
2019-05-24 13:51:46 -0500

    generalized data

---
+++ Keras_Fine_Tune_VGG_16_Liver.py
@@ -0,0 +1,103 @@
+from keras.applications import imagenet_utils
+from keras import models
+from keras.layers import Conv2D, Activation, BatchNormalization, Input, MaxPooling2D, UpSampling2D, Concatenate
+from keras.models import Model
+from keras.utils import get_file
+preprocess_input = imagenet_utils.preprocess_input
+
+WEIGHTS_PATH = ('https://github.com/fchollet/deep-learning-models/'
+                'releases/download/v0.1/'
+                'vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+WEIGHTS_PATH_NO_TOP = ('https://github.com/fchollet/deep-learning-models/'
+                       'releases/download/v0.1/'
+                       'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
+
+
+class Unet(object):
+    def define_filters(self, filters=(3,3)):
+        self.filters = filters
+
+    def define_activation(self, activation='relu'):
+        self.activation = activation
+
+    def define_pool_size(self, pool_size=(2,2)):
+        self.pool_size = pool_size
+
+    def conv_block(self, output_size, x, name, strides=1):
+        x = Conv2D(output_size, self.filters, activation=None, padding='same',
+                   name=name, strides=strides)(x)
+        x = Activation(self.activation, name=name+'_activation')(x)
+        # x = BatchNormalization()(x)
+        return x
+
+    def pool_block(self, x, name):
+        return MaxPooling2D(self.pool_size,name=name)(x)
+
+    def make_model(self):
+        pass
+
+
+class VGG_16(Unet):
+    def __init__(self, network=None):
+        self.define_filters((3,3))
+        self.define_pool_size((2,2))
+        self.define_activation('relu')
+        self.network = network
+
+    def make_network(self):
+        filters = 64
+        network = {}
+        for block in range(5):
+            network['Layer_' + str(block)] = {'Encoding': [],'Decoding':[]}
+            conv_blocks = 2 if block < 2 else 3
+            for conv_block in range(conv_blocks):
+                network['Layer_' + str(block)]['Encoding'].append(filters)
+            filters *= 2 if filters < 512 else 1
+        self.network = network
+
+    def make_model(self):
+        image_input_primary = x = Input(shape=(None, None, 3), name='Image_Input')
+        layer_vals = {}
+        layer_order = []
+        layer_index = 0
+        for i, layer in enumerate(self.network):
+            key = 'Encoding'
+            for ii, filters in enumerate(self.network[layer][key]):
+                self.desc = layer + '_Encoding_Conv' + str(ii)
+                x = self.conv_block(filters,x=x,name='block'+str(i+1)+'_conv'+str(ii+1))
+            layer_vals[layer_index] = x
+            layer_index += 1
+            if layer != 'Layer_4':
+                layer_order.append(layer)
+                x = self.pool_block(x, name='block'+str(i+1)+'_pool')
+        layer_order.reverse()
+        layer_index -= 1
+        for i, layer in enumerate(layer_order):
+            print(layer)
+            layer_index -= 1
+            if 'Decoding' not in self.network[layer]:
+                continue
+            all_filters = self.network[layer]['Decoding']
+            x = UpSampling2D(size=self.pool_size, name='Upsampling' + str(i) + '_UNet')(x)
+            x = Concatenate(name='concat' + str(i) + '_Unet')([x, layer_vals[layer_index]])
+            for i in range(len(all_filters)):
+                self.desc = layer + '_Decoding_Conv' + str(i)
+                x = self.conv_block(all_filters[i], x, self.desc)
+
+        x = self.conv_block(16, x=x, name='Before_output')
+        x = Conv2D(2, kernel_size=(1,1), name='Output', activation='softmax')(x)
+
+        model = Model(inputs=[image_input_primary], outputs=[x],name='VGG16_FineTune')
+        self.created_model = model
+
+    def load_weights(self):
+        weights_path = get_file(
+            'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+            WEIGHTS_PATH_NO_TOP,
+            cache_subdir='models',
+            file_hash='6d6bbae143d832006294945121d1f1fc')
+        self.created_model.load_weights(weights_path, by_name=True)
+
+
+if __name__ == '__main__':
+    xxx = 1
---
+++ Make_VGG_Model.py
@@ -0,0 +1,16 @@
+from Keras_Fine_Tune_VGG_16_Liver import VGG_16
+
+my_path = r'Y:\CNN\VGG_16\VGG16_UNet.hdf5'
+network = {'Layer_0': {'Encoding': [64, 64], 'Decoding': [64, 32]},
+           'Layer_1': {'Encoding': [128, 128], 'Decoding': [128]},
+           'Layer_2': {'Encoding': [256, 256, 256], 'Decoding': [256]},
+           'Layer_3': {'Encoding': [512, 512, 512], 'Decoding': [512]},
+           'Layer_4': {'Encoding': [512, 512, 512]}}
+'''
+The encoding part of the network should not be changed, as it will not load in the VGG_16 pre-trained weights otherwise
+'''
+VGG_model = VGG_16(network=network)
+VGG_model.make_model()
+VGG_model.load_weights()
+new_model = VGG_model.created_model
+VGG_model.created_model.save(my_path)
